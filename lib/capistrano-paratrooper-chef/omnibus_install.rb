3aeb3e3e21e89e2481314a3bc18b829e34e70f7e
Takeshi KOMIYA <i.tkomiya@gmail.com>
2013-04-30 16:49:02 +0900

    Use /opt/chef/bin/chef-solo as default if paratrooper-chef/omnibus_install is loaded

--- lib/capistrano-paratrooper-chef/omnibus_install.rb
+++ lib/capistrano-paratrooper-chef/omnibus_install.rb
@@ -3,6 +3,8 @@ require 'capistrano-paratrooper-chef'
 Capistrano::Configuration.instance.load do
   namespace :paratrooper do
     namespace :chef do
+      set :chef_solo_path, "/opt/chef/bin/chef-solo"
+
       desc "Installs chef (by omnibus installer)"
       task :install_omnibus_chef do
         run "curl -L http://www.opscode.com/chef/install.sh | #{top.sudo} bash"
